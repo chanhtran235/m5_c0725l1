857d376ebb70a7b8ac4f995d4de0b110f3ced644
chanhtv235 <chanhtv235@gmail.com>
2026-05-20 19:59:42 +0700

    hook

--- ss2_tong_quan/demo1/src/App.jsx
+++ ss2_tong_quan/demo1/src/App.jsx
@@ -4,7 +4,7 @@ import {getAll} from "./service/studentService.js";
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
 import HeaderComponent from "./components/HeaderComponent.jsx";
-import ListComponent from "./class_component/ListComponent.jsx";
+import ListComponent from "./components/ListComponent.jsx";
 
 function App() {
   return (
---
+++ ss2_tong_quan/demo1/src/components/ListComponent.jsx
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from "react";
+import {getAll} from "../service/studentService.js";
+import DeleteModal from "./DeleteModal.jsx";
+
+const ListComponent =()=>{
+
+    const [deleteStudent,setDeleteStudent] = useState({
+        id:"",
+        name:""
+    });
+
+    const [studentList,setStudentList] = useState([]);
+    const [isShowModal,setIsShowModal] = useState(false);
+    const [reloading,setReloading] = useState(false);
+    useEffect(() => {
+        console.log('-------useEffect-------------')
+        setStudentList([...getAll()]);
+    },[reloading]);
+    const closeModal = ()=>{
+       setIsShowModal(false);
+    }
+
+    const handleOpenModal =(student)=>{
+        setIsShowModal(true);
+        setDeleteStudent(student);
+
+    }
+
+    return(
+        <>
+            {console.log("---------render list--------------")}
+            <h1>Danh sách sinh viên</h1>
+            <table className={'table table-striped table-dark'}>
+                <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>ID</th>
+                    <th>Tên</th>
+                    <th>Xoá</th>
+                </tr>
+                </thead>
+                <tbody>
+                {studentList.map((student, i) =>
+                    <tr key={student.id}>
+                        <td>{i + 1}</td>
+                        <td>{student.id}</td>
+                        <td>{student.name}</td>
+                        <td>
+                            <button onClick={()=>{
+                                handleOpenModal(student)
+                            }} className={"btn btn-sm btn-danger"}>Xoá</button>
+                        </td>
+                    </tr>)}
+                </tbody>
+            </table>
+            <DeleteModal deleteStudent = {deleteStudent}
+                         isShow = {isShowModal}
+                         closeModal = {closeModal}
+                         setReloading = {setReloading}
+            />
+        </>
+    );
+}
+export default ListComponent;
